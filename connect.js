3eaf30389888b69d076c1cae0d1192ec2c03c742
Isabel Limas <ilimasc3@gmail.com>
2020-02-08 17:41:22 -0500

    added comments for loop set start value to 0, started function update board

--- connect.js
+++ connect.js
@@ -2,7 +2,7 @@ console.log('hello, you are connected');
 let gameStarted = false;
 let currentPlayer = 0;
 let gameBoard = [];
-// check if game has started
+// check if game has started 
 function startGame(){
     if (gameStarted == true){
         return 0;
@@ -10,10 +10,15 @@ function startGame(){
     else{
         gameStarted === true;
     }
+    // set all values of start gameboard to 0
     for(let x=0; x <= 5; x++){
         gameBoard[x] = [];
         for (let y=0; y<=6; y++){
             gameBoard[x][y] = 0;
         }
     }
+}
+// display updated board after a piece is dropped 
+function reDrawBoard(){
+
 }
