dd2f82e2c056c06947dc781a988df71ddcffed29
Isabel Limas <ilimasc3@gmail.com>
2020-02-08 17:01:24 -0500

    creating function to track if new game is starting

--- connect.js
+++ connect.js
@@ -2,3 +2,8 @@ console.log('hello, you are connected');
 let gameStarted = false;
 let currentPlayer = 0;
 let gameBoard = [];
+function startGame(){
+    if (gameStarted == true){
+        return 0;
+    }
+}
