dac0fc5f0c4a2bb043610a3d6791f44645e26720
Isabel Limas <ilimasc3@gmail.com>
2020-02-08 17:21:11 -0500

    setting function for values of empty holes

--- connect.js
+++ connect.js
@@ -2,8 +2,15 @@ console.log('hello, you are connected');
 let gameStarted = false;
 let currentPlayer = 0;
 let gameBoard = [];
+// check if game has started
 function startGame(){
     if (gameStarted == true){
         return 0;
+    }
+    else{
+        gameStarted === true;
+    }
+    for(let i=0, i <= 5; i++){
+        gameBoard[i] = [];
     }
 }
