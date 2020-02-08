836255b88c32ce1371de498b62264d7963e38058
Isabel Limas <ilimasc3@gmail.com>
2020-02-08 17:57:04 -0500

    added logic and comments for reDrawBoard function

--- connect.js
+++ connect.js
@@ -20,5 +20,17 @@ function startGame(){
 }
 // display updated board after a piece is dropped 
 function reDrawBoard(){
-
+    // loop through the entire gameBoard 
+    for(let x=0; x <= 5; x++){
+        for (let y=0; y<=6; y++){
+            // if a player, 1 or 2, dropped their piece the value change will add class player 1 or 2.
+            // this will change the color to reflect who owns the hole on the board.
+            if(gameBoard[x][y] == 1){
+                document.getElementById("hole-" + x + "-" + y).classList.add("player1");
+            }
+            else if( gameBoard[x][y] == 2){
+                document.getElementById("hole-" + x + "-" + y).classList.add("player2");
+            }
+        }
+    }
 }
