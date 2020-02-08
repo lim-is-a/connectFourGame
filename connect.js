35728d4045531993a523415899ccc79012a24094
Isabel Limas <ilimasc3@gmail.com>
2020-02-08 18:16:53 -0500

    trying to figure out game piece drop logic

--- connect.js
+++ connect.js
@@ -2,7 +2,7 @@ console.log('hello, you are connected');
 let gameStarted = false;
 let currentPlayer = 0;
 let gameBoard = [];
-// check if game has started 
+// function to check if game has started 
 function startGame(){
     if (gameStarted == true){
         return 0;
@@ -10,18 +10,18 @@ function startGame(){
     else{
         gameStarted === true;
     }
-    // set all values of start gameboard to 0
+    // set all values of starting gameboard to 0
     for(let x=0; x <= 5; x++){
         gameBoard[x] = [];
         for (let y=0; y<=6; y++){
             gameBoard[x][y] = 0;
         }
     }
-    // call function to redraw board and set current player as player 1
+    // callback function to redraw board and set current player as player 1
     reDrawBoard();
     currentPlayer = 1;
 }
-// display updated board after a piece is dropped 
+// function to display updated board after a piece is dropped 
 function reDrawBoard(){
     // loop through the entire gameBoard 
     for(let x=0; x <= 5; x++){
@@ -34,6 +34,17 @@ function reDrawBoard(){
             else if( gameBoard[x][y] == 2){
                 document.getElementById("hole-" + x + "-" + y).classList.add("player2");
             }
+        }
+    }
+}
+// function for current player dropping game piece in empty game hole
+function dropGamePiece(y){
+    // loop through each row starting at the last row
+    for(let x=5; x >= 0, x--){
+        // if no game piece is there, then place current player game piece
+        if(gameBoard[x][y] == 0){
+            gameBoard[x][y] = currentPlayer; 
+            reDrawBoard();
         }
     }
 }
