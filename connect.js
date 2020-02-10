b5bcecce987e710f474da4a77abdea7b2464de59
Isabel Limas <ilimasc3@gmail.com>
2020-02-09 20:25:22 -0500

    added resetBoard() to clear gameBoard, also added logic must start new game if player wins

--- connect.js
+++ connect.js
@@ -9,16 +9,18 @@ let player2TotalScore = 0;
 
 // get start button to start game
 let begin = document.getElementsByClassName('start')[0];
-begin.addEventListener('click', startGame());
+begin.addEventListener('click', startGame);
 
 // function to check if game has started 
 function startGame(){
-    if (gameStarted == true){
+    if (gameStarted === true){
         return 0;
     }
     else{
-        gameStarted === true;
+        gameStarted = true;
     }
+    // clean board for new game
+    resetBoard()
     // set all values of starting gameboard to 0
     for(let x=0; x <= 5; x++){
         gameBoard[x] = [];
@@ -48,31 +50,36 @@ function reDrawBoard(){
 }
 // function for current player dropping game piece in empty game hole
 function dropGamePiece(y){
-
-    console.log('dropped piece in column: ' + y)
+    if(gameStarted){
+        console.log('dropped piece in column: ' + y)
     // loop through each row starting at the last row
-    for(let x=5; x >= 0; x--){
-        // if no game piece is there, then place current player game piece
-        if(gameBoard[x][y] == 0){
-            gameBoard[x][y] = currentPlayer; 
-            reDrawBoard();
-            checkHorizontal4();
-            checkVertical4();
-            // check which player is currently playing and change it to the other player
-            // if its player 1 then change to 2
-            if (currentPlayer == 1){
-                currentPlayer = 2;
-                console.log("player 2's turn");
+        for(let x=5; x >= 0; x--){
+            // if no game piece is there, then place current player game piece
+            if(gameBoard[x][y] == 0){
+                gameBoard[x][y] = currentPlayer; 
+                reDrawBoard();
+                checkHorizontal4();
+                checkVertical4();
+                // check which player is currently playing and change it to the other player
+                // if its player 1 then change to 2
+                if (currentPlayer == 1){
+                    currentPlayer = 2;
+                    console.log("player 2's turn");
+                }
+                // if its not player 1 then its player 2, so change to player 1.
+                else{
+                    currentPlayer = 1;
+                    console.log("player 1's turn");
+                }
+                // return something to stop checking for empty spots once player drops piece
+                return 0;
             }
-            // if its not player 1 then its player 2, so change to player 1.
-            else{
-                currentPlayer = 1;
-                console.log("player 1's turn");
-            }
-            // return something to stop checking for empty spots once player drops piece
-            return 0;
         }
     }
+    else{
+        alert("You must start a new game!")
+    }
+    
 }
 // make a function to check if any player has won by 4 game pieces in a row horizontal
 let player1Qty=0;
@@ -87,11 +94,13 @@ function checkHorizontal4(){
                 player1TotalScore ++;
                 updateTotalScores()
                 alert('player 1 wins!')
+                gameStarted = false
                 return 0;
             }else if(player2Qty === 4){
                 player2TotalScore ++;
                 updateTotalScores()
                 alert('player 2 wins!')
+                gameStarted = false
                 return 0;
             }
             if(gameBoard[x][y] === 1){
@@ -119,24 +128,26 @@ function checkVertical4(){
                 player1TotalScore ++;
                 updateTotalScores()
                 alert('player 1 wins!')
+                gameStarted = false
                 return 0;
             }else if(player2Qty === 4){
                 player2TotalScore ++;
                 updateTotalScores()
                 alert('player 2 wins!')
+                gameStarted = false
                 return 0;
             }
             if(gameBoard[x][y] === 1){
                 player2Qty = 0;
                 player1Qty ++;
-                console.log('player2 qty: ' +player2Qty )
-                console.log('player1 qty: ' +player1Qty )
+                // console.log('player2 qty: ' +player2Qty )
+                // console.log('player1 qty: ' +player1Qty )
             } 
             else if (gameBoard[x][y] === 2){
                 player1Qty = 0;
                 player2Qty ++;
-                console.log('player2 qty: ' +player2Qty )
-                console.log('player1 qty: ' +player1Qty )
+                // console.log('player2 qty: ' +player2Qty )
+                // console.log('player1 qty: ' +player1Qty )
             }
         }
     }
@@ -146,4 +157,16 @@ function checkVertical4(){
 function updateTotalScores(){
     document.getElementById('p1ScoreTotal').innerHTML = "Player 1: " + player1TotalScore
     document.getElementById('p2ScoreTotal').innerHTML = "Player 2: " + player2TotalScore
+}
+// clear out board
+function resetBoard(){
+    // loop through the entire gameBoard 
+    for(let x=0; x <= 5; x++){
+        for (let y=0; y<=6; y++){
+            // if a player, 1 or 2, dropped their piece the value change will add class player 1 or 2.
+            // this will change the color to reflect who owns the hole on the board.
+                document.getElementById("hole-" + x + "-" + y).classList.remove("player1");
+                document.getElementById("hole-" + x + "-" + y).classList.remove("player2");
+        }
+    }
 }
